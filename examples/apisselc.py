704300aa16ef624c2af7525e477608205272d540
quelgeek <roy.hann@rationalcommerce.com>
2023-05-12 08:38:05 +0100

    Add files via upload

---
+++ examples/apisselc.py
@@ -0,0 +1,226 @@
+#!/usr/bin/env python
+
+##  Copyright (c) 2023 Roy Hann
+
+from pyngres import *
+import ctypes
+import sys
+
+
+def IIdemo_init():
+    '''Initialize API access'''
+
+    inp = IIAPI_INITPARM()
+
+    print('IIdemo_init: initializing API')
+    inp.in_version = IIAPI_VERSION_11
+    inp.in_timeout = -1
+
+    IIapi_initialize(inp)
+    status = inp.in_status
+    if status != IIAPI_ST_SUCCESS:
+        print(f'{status=} ({IIAPI_ST_MSG[status]})')
+        quit()
+
+    envHandle = inp.in_envHandle
+    return envHandle
+
+
+def IIdemo_term(envHandle):
+    '''Terminate API access'''
+
+    rep = IIAPI_RELENVPARM()
+    tmp = IIAPI_TERMPARM()
+
+    rep.re_envHandle = envHandle
+    print('IIdemo_term: releasing environment resources')
+    IIapi_releaseEnv(rep)
+
+    print('IIdemo_term: shutting down API')
+    IIapi_terminate(tmp)
+
+
+def IIdemo_conn(target, envHandle):
+    '''open connection with target Database'''
+
+    cop = IIAPI_CONNPARM()
+    wtp = IIAPI_WAITPARM()
+    wtp.wt_timeout = -1
+
+    print('IIdemo_conn: establishing connection')
+    cop.co_genParm.gp_callback = None
+    cop.co_genParm.gp_closure = None
+    cop.co_target = target
+    cop.co_connHandle = envHandle
+    cop.co_type = IIAPI_CT_SQL
+    cop.co_tranHandle = None
+    cop.co_username = None
+    cop.co_password = None
+    cop.co_timeout = -1
+
+    IIapi_connect(cop)
+
+    while not cop.co_genParm.gp_completed:
+        IIapi_wait(wtp)
+
+    connHandle = cop.co_connHandle
+    return connHandle
+
+
+def IIdemo_disconn(connHandle):
+    '''Release DBMS connection'''
+
+    dcp = IIAPI_DISCONNPARM()
+    wtp = IIAPI_WAITPARM()
+    wtp.wt_timeout = -1
+
+    print('IIdemo_disconn: releasing connection')
+    dcp.dc_genParm.gp_callback = None
+    dcp.dc_genParm.gp_closure = None
+    dcp.dc_connHandle = connHandle
+
+    IIapi_disconnect(dcp)
+
+    while not dcp.dc_genParm.gp_completed:
+        IIapi_wait(wtp)
+
+    connHandle = None
+    return connHandle
+
+
+def IIdemo_rollback(tranHandle):
+    '''rollback current transaction and reset transaction handle'''
+
+    rbp = IIAPI_ROLLBACKPARM()
+    wtp = IIAPI_WAITPARM()
+    wtp.wt_timeout = -1
+
+    print('IIdemo_rollback: rolling back transaction')
+    rbp.rb_genParm.gp_callback = None
+    rbp.rb_genParm.gp_closure = None
+    rbp.rb_tranHandle = tranHandle
+    rbp.rb_savePointHandle = None
+
+    IIapi_rollback(rbp)
+
+    while not rbp.rb_genParm.gp_completed:
+        IIapi_wait(wtp)
+
+    tranHandle = None
+    return tranHandle
+
+
+##  this is the main body of the demonstration code
+argv = sys.argv
+script = argv[0]
+if len(argv) != 2:
+    print(f'usage: python {script} [vnode::]dbname[/server_class]')
+    quit()
+dbtarget = argv[1]
+target = dbtarget.encode()
+
+queryText = b'SELECT table_name FROM iitables'
+ROW_COUNT = 5
+dataArray = (IIAPI_DATAVALUE * ROW_COUNT)()
+dataArrayPtr = ctypes.cast(dataArray, ctypes.POINTER(IIAPI_DATAVALUE))
+varvalue = []
+for i in range(ROW_COUNT):
+    varvalue.append(ctypes.c_buffer(256))
+
+qyp = IIAPI_QUERYPARM()
+gdp = IIAPI_GETDESCRPARM()
+gcp = IIAPI_GETCOLPARM()
+gqp = IIAPI_GETQINFOPARM()
+clp = IIAPI_CLOSEPARM()
+wtp = IIAPI_WAITPARM()
+wtp.wt_timeout = -1
+
+
+envHandle = IIdemo_init()
+connHandle = IIdemo_conn(target, envHandle)
+tranHandle = None
+
+##  open cursor
+print(f'{script}: opening cursor')
+
+qyp.qy_genParm.gp_callback = None
+qyp.qy_genParm.gp_closure = None
+qyp.qy_connHandle = connHandle
+qyp.qy_queryType = IIAPI_QT_OPEN
+qyp.qy_queryText = queryText
+qyp.qy_parameters = False
+qyp.qy_tranHandle = tranHandle
+qyp.qy_stmtHandle = None
+
+IIapi_query(qyp)
+
+while not qyp.qy_genParm.gp_completed:
+    IIapi_wait(wtp)
+
+tranHandle = qyp.qy_tranHandle
+stmtHandle = qyp.qy_stmtHandle
+
+##  get query result descriptors
+print(f'{script}: get descriptors')
+
+gdp.gd_genParm.gp_callback = None
+gdp.gd_genParm.gp_closure = None
+gdp.gd_stmtHandle = stmtHandle
+gdp.gd_descriptorCount = 0
+gdp.gd_descriptor = None
+
+IIapi_getDescriptor(gdp)
+
+while not gdp.gd_genParm.gp_completed:
+    IIapi_wait(wtp)
+
+##  get query results
+print(f'{script}: get results')
+
+gcp.gc_genParm.gp_callback = None
+gcp.gc_genParm.gp_closure = None
+gcp.gc_rowCount = ROW_COUNT
+gcp.gc_columnCount = 1
+gcp.gc_columnData = dataArrayPtr
+gcp.gc_stmtHandle = stmtHandle
+gcp.gc_moreSegments = 0
+
+for i in range(ROW_COUNT):
+    gcp.gc_columnData[i].dv_value = ctypes.addressof(varvalue[i])
+
+IIapi_getColumns(gcp)
+
+while not gcp.gc_genParm.gp_completed:
+    IIapi_wait(wtp)
+
+nrows = gcp.gc_rowsReturned
+for i in range(nrows):
+    columName = gdp.gd_descriptor[0].ds_columnName
+    value = ctypes.string_at(varvalue[i], size=256)
+    print(f'{columName=} {value}')
+
+##  fetch result info
+gqp.gq_genParm.gp_callback = None
+gqp.gq_genParm.gp_closure = None
+gqp.gq_stmtHandle = stmtHandle
+
+IIapi_getQueryInfo(gqp)
+
+while not gqp.gq_genParm.gp_completed:
+    IIapi_wait(wtp)
+
+##  free query resources
+print(f'{script}: close cursor')
+
+clp.cl_genParm.gp_callback = None
+clp.cl_genParm.gp_closure = None
+clp.cl_stmtHandle = stmtHandle
+
+IIapi_close(clp)
+
+while not clp.cl_genParm.gp_completed:
+    IIapi_wait(wtp)
+
+IIdemo_disconn(connHandle)
+IIdemo_term(envHandle)
+quit()
